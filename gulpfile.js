de389b20819b3ad6396b96989b57e0a127f5c1f8
didaKtik <pierre.mohr.durdez@gmail.com>
2016-03-14 15:19:40 +0100

    chore: Add browserify to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gls = require('gulp-live-server');
+var browserify = require('gulp-browserify');
+
+gulp.task('browserify', function() {
+  return gulp.
+    src('./public/js/index.js').
+    pipe(browserify()).
+    pipe(gulp.dest('./public/js/bin'));
+});
+
+// Live reload of Chrome in development
+gulp.task('serve', ['browserify'], function() {
+  var serverScript = './bin/www';
+  // Run the given script as a server 
+  var server = gls.new(serverScript);
+  // The magic is here: the server is a child process, 
+  // so it is possible to continue watching for changes
+  server.start();
+
+  gulp.watch(['./public/js/*.js'], ['browserify']);
+
+  gulp.watch(['./**/*.js', './**/*.html', '!node_modules'], function (file) {
+    server.notify.apply(server, [file]);
+  });
+  
+  gulp.watch(serverScript, function() {
+    server.start.bind(server)(); //restart server
+  }); 
+});
 
 gulp.task('test', function() {
   var error = false;
@@ -21,23 +49,4 @@ gulp.task('test', function() {
     });
 });
 
-// Live reload of Chrome for development
-var serverScript = './bin/www';
 
-gulp.task('serve', function() {
-  // Run the given script as a server 
-  var server = gls.new(serverScript);
-  // The magic is here: the server is a child process, 
-  // so it is possible to continue watching for changes
-  server.start();
-
-  gulp.watch(['./**/*.js', './**/*.html', '!node_modules'], function (file) {
-    server.notify.apply(server, [file]);
-  });
-  gulp.watch(serverScript, server.start.bind(server)); //restart my server 
-  
-  // Note: try wrapping in a function if getting an error like `TypeError: Bad argument at TypeError (native) at ChildProcess.spawn` 
-  // gulp.watch(serverScript, function() {
-  //   server.start.bind(server)()
-  // });
-});
